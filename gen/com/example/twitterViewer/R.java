2bd3e171ba3b103dd7b2676370c3e80070e30e2b
Samotiuc Igor <igori.s@mail.ru>
2013-04-13 19:16:17 +0300

    Test3

--- gen/com/example/twitterViewer/R.java
+++ gen/com/example/twitterViewer/R.java
@@ -14,15 +14,23 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int From=0x7f060006;
+        public static final int btnPrevious=0x7f060003;
         public static final int btnRefreshTimeLine=0x7f060002;
         public static final int btnlogIn=0x7f060001;
+        public static final int button2=0x7f060004;
+        public static final int description=0x7f060008;
+        public static final int icon=0x7f060005;
         public static final int listView1=0x7f060000;
-        public static final int textView1=0x7f060003;
-        public static final int webView1=0x7f060004;
+        public static final int subject=0x7f060007;
+        public static final int textView1=0x7f06000a;
+        public static final int time=0x7f060009;
+        public static final int webView1=0x7f06000b;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int webview=0x7f030001;
+        public static final int list_item_message=0x7f030001;
+        public static final int webview=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
